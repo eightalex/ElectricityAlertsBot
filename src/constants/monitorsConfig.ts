c441fb60396363489645fe126cfe8984a851101f
eightalex <eight.alex@gmail.com>
2024-06-10 17:20:50 +0300

    feature(monitorsConfig): updated config

--- src/constants/monitorsConfig.ts
+++ src/constants/monitorsConfig.ts
@@ -2,37 +2,23 @@ import {BotConfigType} from '../../types/BotConfigType';
 
 const MONITORS_MAP = {
     UNDERNET_25A_2_SECTION: 793214785,
-    TOILET_SOCKET_4_SECTION: 793535128,
+    BOILER_SOCKET_4_SECTION: 793535128,
 };
 
 export const MONITORS_CONFIG: BotConfigType[] = [
     {
         ID: 1,
-        NAME: 'kombinatna25a_2_section',
-        MONITORS: [MONITORS_MAP.UNDERNET_25A_2_SECTION],
+        NAME: 'kombinatna25a',
+        MONITORS: [
+            MONITORS_MAP.UNDERNET_25A_2_SECTION,
+            MONITORS_MAP.BOILER_SOCKET_4_SECTION,
+        ],
         TELEGRAM_CHATS: [
             {chat_id: '@kombinatna_alerts'},
             {chat_id: '-1001811166479', message_thread_id: 567},
         ],
         STATISTICS: {
             INFORM_TIME: '23:58',
-        },
-        MESSAGE: {
-            AVAILABLE: '🟢 З\'явилось світло в 2 секції',
-            UNAVAILABLE: '⚫️ Зникло світло в 2 секції',
-        },
-    },
-    {
-        ID: 2,
-        NAME: 'kombinatna25a_4_section',
-        MONITORS: [MONITORS_MAP.TOILET_SOCKET_4_SECTION],
-        TELEGRAM_CHATS: [
-            {chat_id: '@kombinatna_alerts'},
-            {chat_id: '-1001811166479', message_thread_id: 567},
-        ],
-        MESSAGE: {
-            AVAILABLE: '🟢 З\'явилось світло в 4 секції',
-            UNAVAILABLE: '⚫️ Зникло світло в 4 секції',
         },
     },
 ];
@@ -56,7 +42,7 @@ export const MONITORS_CONFIG_DEV: BotConfigType[] = [
     {
         ID: 2,
         NAME: 'kombinatna25a_4_section',
-        MONITORS: [MONITORS_MAP.TOILET_SOCKET_4_SECTION],
+        MONITORS: [MONITORS_MAP.BOILER_SOCKET_4_SECTION],
         TELEGRAM_CHATS: [
             {chat_id: '@kombinatna_test_alerts'},
         ],
