76f9a12724f27fdd4ca55b1ea9bfaf89da944e98
eightalex <eight.alex@gmail.com>
2024-08-28 17:01:55 +0300

    style(config): updated config

--- src/constants/monitorsConfig.ts
+++ src/constants/monitorsConfig.ts
@@ -19,26 +19,37 @@ export const IDS = {
 
 export const MONITORS_CONFIG: BotConfigType[] = [
     {
-        ID: IDS.KOMBINATNA_25A,
-        NAME: 'kombinatna25a',
+        ID: IDS.KOMBINATNA25A_4_SECTION,
+        NAME: 'kombinatna25a_4_section',
         REGION: REGION.KYIV,
         GROUP: 5,
-        MONITORS: [
-            MONITORS_MAP.UNDERNET_25A_2_SECTION,
-            MONITORS_MAP.BOILER_SOCKET_4_SECTION,
-        ],
+        MONITORS: [MONITORS_MAP.BOILER_SOCKET_4_SECTION],
         TELEGRAM_CHATS: [
             {chat_id: '@kombinatna_alerts'},
             {chat_id: '-1001811166479', message_thread_id: 567},
         ],
-        STATISTICS: {
-            INFORM_TIME: '23:58',
+        MESSAGE: {
+            AVAILABLE: '🟢 З\'явилось світло (ТП 1)',
+            UNAVAILABLE: '⚫️ Зникло світло (ТП 1)',
         },
         SCHEDULE: {
             INFORM_TIME: '21:00',
         },
         FUTURE_OUTAGE: {
             MINUTES: 30,
+        },
+    },
+    {
+        ID: IDS.KOMBINATNA25A_2_SECTION,
+        NAME: 'kombinatna25a_2_section',
+        MONITORS: [MONITORS_MAP.UNDERNET_25A_2_SECTION],
+        TELEGRAM_CHATS: [
+            {chat_id: '@kombinatna_alerts'},
+            {chat_id: '-1001811166479', message_thread_id: 567},
+        ],
+        MESSAGE: {
+            AVAILABLE: '🟢 З\'явилось світло (ТП 2)',
+            UNAVAILABLE: '⚫️ Зникло світло (ТП 2)',
         },
     },
     {
